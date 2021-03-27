6f3bb499467a9ac5b72595212ccb8dcc17c57531
Giacomo Debidda <jackdebidda@gmail.com>
2021-03-27 21:47:57 +0100

    Update workbox config: avoid precaching too many resources

--- workbox-config.js
+++ workbox-config.js
@@ -4,20 +4,43 @@ module.exports = {
   cleanupOutdatedCaches: true,
   globDirectory: '_site/',
   globPatterns: [
-    '**/*.{css,html,gif,ico,jpg,js,json,png,svg,txt,xml,webmanifest,webp,woff,woff2}'
+    // precache all the main pages
+    '{404,index,success}.html',
+    '{blog,contact,projects,tags}/index.html',
+    // precaching all blog posts is probably too much. Maybe precache only the most popular blog posts? Or the most recent ones?
+    // '**/posts/*/index.html'
+    // precache local JS files
+    '**/*.js',
+    // precache local CSS files
+    'assets/css/*.css',
+    // precache local fonts
+    'assets/fonts/*.{woff,woff2}',
+    // precache local images (but I host most of my images on Cloudinary, so there aren't many images hosted on this origin)
+    '**/*.{avif,gif,ico,jpg,png,svg,webp}',
+    // I still don't know whether precaching the RSS feed (it's ~1MB) and the sitemap (it's ~16KB) is a good idea or not.
+    '{feed,sitemap}.xml'
+    // should I precache JSON? txt files (e.g. robots.txt)? webmanifest?
+    // '**/*.{json,txt,webmanifest}'
   ],
   ignoreURLParametersMatching: [
     new RegExp(/^utm_/, 'i'),
     new RegExp(/^fbclid$/, 'i')
   ],
   runtimeCaching: [
+    // Google fonts
     {
       handler: 'StaleWhileRevalidate',
       urlPattern: /^https?:\/\/fonts\.googleapis\.com/
     },
+    // Google fonts (again)
     {
       handler: 'StaleWhileRevalidate',
       urlPattern: /^https?:\/\/fonts\.gstatic\.com/
+    },
+    // images and media hosted on Cloudinary
+    {
+      handler: 'StaleWhileRevalidate',
+      urlPattern: /^https?:\/\/res\.cloudinary\.com/
     }
   ],
   // skipWaiting: true,
