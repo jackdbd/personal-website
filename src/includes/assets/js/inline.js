fa8e0219c989e95d7acc98580c56e480d2cd898b
Giacomo Debidda <jackdebidda@gmail.com>
2021-04-10 00:27:30 +0200

    Fix cycleTheme

--- src/includes/assets/js/inline.js
+++ src/includes/assets/js/inline.js
@@ -1,13 +1,18 @@
 (function iife() {
   console.log('IIFE inline.js');
+  const DATA_ATTRIBUTE = 'data-theme';
+  const LOCAL_STORAGE_THEME_KEY = 'theme';
+  const DEFAULT_THEME = 'default';
+
   const setTheme = (theme) => {
-    document.documentElement.setAttribute('theme', theme);
-    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute(DATA_ATTRIBUTE, theme);
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
   };
 
   const makeCycleTheme = (m) => {
     return function cycleTheme() {
-      const currentTheme = localStorage.getItem('theme') || 'default';
+      const currentTheme =
+        localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME;
       const nextTheme = m[currentTheme];
       setTheme(nextTheme);
     };
@@ -20,7 +25,8 @@
     dark: 'default'
   });
 
-  const initialTheme = localStorage.getItem('theme') || 'default';
+  const initialTheme =
+    localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME;
   setTheme(initialTheme);
   // setTheme('default');
   // setTheme('dark');
