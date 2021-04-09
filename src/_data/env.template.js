786cb227ed342f6cb03371ae54add06a9c1fd999
Giacomo Debidda <jackdebidda@gmail.com>
2021-04-10 00:17:19 +0200

    Update prebuild script

--- src/_data/env.template.js
+++ src/_data/env.template.js
@@ -3,7 +3,7 @@ module.exports = {
   // 11ty transforms, shortcodes, filters, and collections.
   domain: process.env.DOMAIN,
   environment: process.env.ELEVENTY_ENV,
-  nonce_inline_css: 'NONCE_INLINE_CSS',
-  nonce_inline_js: 'NONCE_INLINE_JS',
-  nonce_sw: 'NONCE_SW'
+  nonce_inline_css: 'INLINE_CSS',
+  nonce_inline_js: 'INLINE_JS',
+  nonce_sw: 'SW_REGISTRATION'
 };
