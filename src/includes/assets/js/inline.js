ce569c99ddc6b9d8bbe7e8f05b4e7323fe3eb182
Giacomo Debidda <jackdebidda@gmail.com>
2021-04-10 15:57:41 +0200

    Avoid inline event handlers and update CSP

--- src/includes/assets/js/inline.js
+++ src/includes/assets/js/inline.js
@@ -1,33 +1,3 @@
 (function iife() {
   // console.log('IIFE inline.js');
-  const DATA_ATTRIBUTE = 'data-theme';
-  const LOCAL_STORAGE_THEME_KEY = 'theme';
-  const DEFAULT_THEME = 'default';
-
-  const setTheme = (theme) => {
-    document.documentElement.setAttribute(DATA_ATTRIBUTE, theme);
-    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
-  };
-
-  const makeCycleTheme = (m) => {
-    return function cycleTheme() {
-      const currentTheme =
-        localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME;
-      const nextTheme = m[currentTheme];
-      setTheme(nextTheme);
-    };
-  };
-
-  window.cycleTheme = makeCycleTheme({
-    default: 'choco',
-    choco: 'teal-purple',
-    'teal-purple': 'dark',
-    dark: 'default'
-  });
-
-  const initialTheme =
-    localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME;
-  setTheme(initialTheme);
-  // setTheme('default');
-  // setTheme('dark');
 })();
