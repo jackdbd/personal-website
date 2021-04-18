1c9866159a00efb59be1579615244ef81330778d
Giacomo Debidda <jackdebidda@gmail.com>
2021-04-18 09:54:58 +0200

    Remove @tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // the moment is not possible to do it with mode: 'jit'
   // https://github.com/tailwindlabs/tailwindcss/issues/4059
   mode: 'jit',
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [],
   // purge works only when NODE_ENV=production (is this still true with @tailwindcss/jit?)
   // https://github.com/tailwindlabs/tailwindcss/pull/1639
   purge: [
