329ca0965c5c2aa2a5f268cfd17997bfcb02f6d3
Giacomo Debidda <jackdebidda@gmail.com>
2021-04-22 01:13:16 +0200

    build-sw with custom Workbox revisions for HTML pages

--- scripts/build-sw.js
+++ scripts/build-sw.js
@@ -1,41 +1,45 @@
 const fs = require('fs');
+const crypto = require('crypto');
 const path = require('path');
 const util = require('util');
-// const readDirAsync = util.promisify(fs.readdir);
 const readFileAsync = util.promisify(fs.readFile);
 const { generateSW } = require('workbox-build');
-const { hashFromString } = require('./hash');
 
-const cacheId = 'giacomodebidda.com';
-const swDest = '_site/sw.js';
+// Factory that returns a function to generate an additional ManifestEntry for
+// the Workbox configuration.
+// https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-build#.ManifestEntry
+const makeManifestEntry = (outputDir, string) => {
+  return async (filepath) => {
+    const sha256Hasher = crypto.createHash('sha256');
+    try {
+      const html = await readFileAsync(filepath, 'utf8');
+      const url = path.relative(outputDir, filepath);
+      sha256Hasher.update(`${html}-${string}`, 'utf-8');
+      return {
+        url,
+        revision: sha256Hasher.digest('base64')
+      };
+    } catch (err) {
+      throw new Error(
+        `Could not read ${filepath}. Double-check the paths in htmlPagesToPrecache\n${err.message}`
+      );
+    }
+  };
+};
 
-// 11ty outputs the generated html pages in this directory.
-const BUILD_ROOT = '_site';
+const makeWorkboxConfig = (options) => {
+  const {
+    additionalManifestEntries = [],
+    cacheId = 'test-cache-id',
+    globDirectory = '_site',
+    swDest = '_site/sw.js'
+  } = options || {};
 
-// https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-build#.generateSW
-const buildSW = async (csp) => {
-  // console.log('🚀 === buildSW === csp', csp);
-  const filepath = path.join(BUILD_ROOT, 'index.html');
-  const html = await readFileAsync(filepath, 'utf8');
-  const indexHtmlRevision = hashFromString(`${html}-${csp}`);
-  console.log(`revision for ${filepath}: ${indexHtmlRevision}`);
-
-  const workboxConfig = {
-    additionalManifestEntries: [
-      {
-        url: 'index.html',
-        revision: indexHtmlRevision
-      }
-    ],
+  return {
+    additionalManifestEntries,
     cacheId,
-    globDirectory: '_site/',
+    globDirectory,
     globPatterns: [
-      // precache all the main pages
-      '404.html',
-      // 'index.html',
-      '{about,blog,contact,projects,styleguide,success,tags}/index.html',
-      // precaching all blog posts is probably too much. Maybe precache only the most popular blog posts? Or the most recent ones?
-      // '**/posts/*/index.html'
       // precache self-hosted static assets (I host most of my images on
       // Cloudinary, so there aren't many images hosted on this origin).
       'assets/css/*.css',
@@ -107,19 +111,97 @@ const buildSW = async (csp) => {
     sourcemap: true,
     swDest
   };
+};
+
+// 11ty outputs the generated html pages in this directory.
+// const BUILD_ROOT = '_site';
+
+// precache all the main pages
+// '{projects,styleguide,success,tags}/index.html',
+// precaching all blog posts is probably too much. Maybe precache only the most
+// popular blog posts? Or the most recent ones?
+// '**/posts/*/index.html'
+
+// https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-build#.generateSW
+const buildSW = async (options) => {
+  const {
+    cacheId = 'test-cache-id',
+    netlifyTomlPath = 'netlify.toml',
+    outputDir = '_site'
+  } = options || {};
+
+  const swDest =
+    options !== undefined && options.swDest !== undefined
+      ? options.swDest
+      : path.join(outputDir, 'sw.js');
+
+  const DEFAULT_HTML_PAGES_TO_PRECACHE = [
+    path.join(outputDir, '404.html'),
+    path.join(outputDir, 'index.html')
+  ];
+
+  const wereHtmlPagesToPrecacheSpecified =
+    options !== undefined && options.htmlPagesToPrecache !== undefined;
+
+  const htmlPagesToPrecache = wereHtmlPagesToPrecacheSpecified
+    ? options.htmlPagesToPrecache
+    : DEFAULT_HTML_PAGES_TO_PRECACHE;
+
+  let string;
+  try {
+    string = await readFileAsync(netlifyTomlPath);
+  } catch (err) {
+    throw new Error(`Could not read ${netlifyTomlPath}\n${err.message}`);
+  }
+
+  const manifestEntry = makeManifestEntry(outputDir, string);
+  const promises = htmlPagesToPrecache.map(manifestEntry);
+
+  let additionalManifestEntries;
+  try {
+    additionalManifestEntries = await Promise.all(promises);
+  } catch (err) {
+    throw new Error(
+      `Could not generate additionalManifestEntries for Workbox config\n${err.message}`
+    );
+  }
+
+  const workboxConfig = makeWorkboxConfig({
+    additionalManifestEntries,
+    cacheId,
+    globDirectory: outputDir,
+    swDest
+  });
 
   try {
-    const value = await generateSW(workboxConfig);
-    const { count, filePaths, size, warnings } = value;
+    const result = await generateSW(workboxConfig);
+    const { count, filePaths, size, warnings } = result;
     const kB = `(${new Intl.NumberFormat('en-US').format(size / 1024)} kB)`;
     console.log(
-      `Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes. ${kB}`
+      `Generated ${swDest}, which will precache ${count} files in cache ${cacheId}, totaling ${size} bytes. ${kB}`
     );
     console.log(`Generated ${filePaths.length} files`, filePaths);
     console.warn('warnings', warnings);
   } catch (err) {
-    console.error(err);
+    throw new Error(`Could not generate ${swDest}\n${err.message}`);
   }
 };
+
+// TODO: write tests for this
+// const CONFIG = {
+//   htmlPagesToPrecache: [
+//     path.join('_site', 'about', 'index.html'),
+//     path.join('_site', 'blog', 'index.html')
+//   ]
+// };
+
+// const CONFIG = {
+//   cacheId: 'giacomodebidda.com',
+// };
+
+// const CONFIG = {};
+// const CONFIG = undefined;
+
+// buildSW(CONFIG).then(console.log).catch(console.error);
 
 module.exports = { buildSW };
