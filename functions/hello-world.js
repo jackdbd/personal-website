cdcbc42bfa7f8ebb1d2ad881d10e8a648599201e
Giacomo Debidda <giacomo@giacomodebidda.com>
2022-07-14 22:26:29 +0200

    redirect to success/failure page after contact form submission

--- functions/hello-world.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet(context) {
-  // console.log('=== context ===', context)
-  // const { env } = context
-  // console.log('=== env ===', env)
-  const resBody = JSON.stringify({ message: `Hello World` }, null, 2)
-  const response = new Response(resBody, { status: 200 })
-  response.headers.set('Content-Type', 'application/json; charset=utf-8')
-  return response
-}
---
+++ functions/notifications.js
@@ -0,0 +1,41 @@
+const sendEmail = async ({ api_key, email, message, name }) => {
+  const strings = [
+    `New contact form submission from <b>${name}</b> (${email})`,
+    message
+  ]
+
+  throw new Error(
+    `sending email using the SendGrid API is not supported at the moment`
+  )
+}
+
+const sendMessageToTelegramChat = async ({
+  chat_id,
+  email,
+  message,
+  name,
+  token
+}) => {
+  const strings = [
+    `New contact form submission from <b>${name}</b> (${email})`,
+    message
+  ]
+
+  const body = {
+    chat_id,
+    disable_notification: false,
+    disable_web_page_preview: true,
+    parse_mode: 'HTML',
+    text: strings.join('\n')
+  }
+
+  return fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-type': `application/json`
+    }
+  })
+}
+
+module.exports = { sendEmail, sendMessageToTelegramChat }
