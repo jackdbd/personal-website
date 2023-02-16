2bf641a478c8c26bf3bdca19c1a43618ab3e9600
Giacomo Debidda <giacomo@giacomodebidda.com>
2023-02-16 16:26:02 +0100

    add 11ty shortcode for BigQuery publicly shared queries

--- 11ty/shortcodes.js
+++ 11ty/shortcodes.js
@@ -13,7 +13,15 @@ const version = () => {
   return String(Date.now())
 }
 
+/**
+ * https://cloud.google.com/bigquery/docs/saving-sharing-queries
+ */
+const bigQueryQueryLink = (href) => {
+  return `<p><a href="${href}" target="_blank" rel="external noopener noreferrer">Here is the query.</a> Try it out!</p>`
+}
+
 module.exports = {
+  bigQueryQueryLink,
   copyright,
   version
 }
