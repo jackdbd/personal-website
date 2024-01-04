a76f4a6f62c6ea5d6cb9fba23277ec63630fc49c
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-01-04 11:25:58 +0100

    run prettier

--- src/includes/assets/js/async-load-prism-theme.js
+++ src/includes/assets/js/async-load-prism-theme.js
@@ -1,20 +1,20 @@
-(function iife() {
-  const ELEMENT_ID = 'prism-stylesheet';
+;(function iife() {
+  const ELEMENT_ID = 'prism-stylesheet'
 
   // The event handler here allows me to avoid having an inline event handler.
   // I don't want any inline Javascript on this site. Infact I allow no inline
   // JS in the script-src-attr directive of my Content-Security-Policy.
   function handleDOMContentLoaded() {
-    const selector = document.getElementById(ELEMENT_ID);
+    const selector = document.getElementById(ELEMENT_ID)
     // The JS code in this file is inlined in the <head> of every page, but not
     // all HTML pages include the Prism.js stylesheet (to avoid making a HTTP
     // request where it's not actually needed). That's why the selector might be
     // absent on the page.
     if (!!selector) {
-      selector.onload = null;
-      selector.rel = 'stylesheet';
+      selector.onload = null
+      selector.rel = 'stylesheet'
     }
   }
 
-  document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
-})();
+  document.addEventListener('DOMContentLoaded', handleDOMContentLoaded)
+})()
--- src/includes/assets/js/theme-switcher.js
+++ src/includes/assets/js/theme-switcher.js
@@ -1,8 +1,8 @@
-(function iife() {
-  const ELEMENT_ID = 'theme-switcher';
-  const LOCAL_STORAGE_THEME_KEY = 'theme';
-  const DATA_ATTRIBUTE = 'data-theme';
-  const DEFAULT_THEME = 'default';
+;(function iife() {
+  const ELEMENT_ID = 'theme-switcher'
+  const LOCAL_STORAGE_THEME_KEY = 'theme'
+  const DATA_ATTRIBUTE = 'data-theme'
+  const DEFAULT_THEME = 'default'
 
   // [current theme]: next theme
   const THEME_CYCLE_MAP = {
@@ -10,30 +10,30 @@
     choco: 'teal-purple',
     'teal-purple': 'dark',
     dark: 'default'
-  };
+  }
 
   document.addEventListener('DOMContentLoaded', function (event) {
-    const selector = document.getElementById(ELEMENT_ID);
+    const selector = document.getElementById(ELEMENT_ID)
 
     const setTheme = (theme) => {
-      document.documentElement.setAttribute(DATA_ATTRIBUTE, theme);
-      localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
-    };
+      document.documentElement.setAttribute(DATA_ATTRIBUTE, theme)
+      localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme)
+    }
 
     const makeCycleTheme = (m) => {
       return function cycleTheme() {
         const currentTheme =
-          localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME;
-        const nextTheme = m[currentTheme];
-        setTheme(nextTheme);
-      };
-    };
+          localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME
+        const nextTheme = m[currentTheme]
+        setTheme(nextTheme)
+      }
+    }
 
     const initialTheme =
-      localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME;
-    setTheme(initialTheme);
+      localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || DEFAULT_THEME
+    setTheme(initialTheme)
 
-    const cycleTheme = makeCycleTheme(THEME_CYCLE_MAP);
-    selector.addEventListener('click', cycleTheme);
-  });
-})();
+    const cycleTheme = makeCycleTheme(THEME_CYCLE_MAP)
+    selector.addEventListener('click', cycleTheme)
+  })
+})()
